bf7ff8b3bcf03b405058fbff4ccf5a3bc3a305c7
Issac Moreno <issaxavier909@gmail.com>
2020-03-19 14:37:01 -0700

    fixed recipe-model

--- schemes/recipe-model.js
+++ schemes/recipe-model.js
@@ -1,22 +1,30 @@
-const db = require("./data/dbConfig")
+const db = require("../data/dbConfig.js");
 
 module.exports = {
-    getRecipes,
-    getShoppingList,
-    getInstructions
-}
-
-function getRecipes() {
-return db("recipes")
+  getRecipes,
+  getShoppingList,
+  getInstructions
 };
 
-function getShoppingList(id) {
-    return db('ingredients')
-    .select("ingredientName", "quantity")
-    .where({recipeId: id})
+function getRecipes() {
+  return db("recipes");
 }
-function getInstructions(id){
-return db('steps')
-    .select("recipeStep")
-    .where({recipeId: id})
+
+function getShoppingList(id) {
+  return db("recipes")
+    .select("ingredients.ingredient_amount", "ingredients.ingredient_name")
+    .join("recipe_ingredients", {
+      "recipe_ingredients.recipe_id": "recipes.id"
+    })
+    .join("ingredients", {
+      "ingredients.id": "recipe_ingredients.ingredient_id"
+    })
+    .where({ "recipes.id": id });
+}
+
+function getInstructions(id) {
+  return db("recipes")
+    .select("steps.step_number", "steps.description")
+    .join("steps", { "recipes.id": "steps.recipe_id" })
+    .where({ "recipes.id": id });
 }
